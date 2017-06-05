5c0588085f41edb72c37fed077c25434c376d642
jferris <jferris@ualberta.ca>
2017-06-04 17:39:04 -0700

    added base classes

---
+++ app/src/main/java/ca/juqe/juqe/BasePresenter.kt
@@ -0,0 +1,11 @@
+package ca.juqe.juqe
+
+/**
+ * Created by Justin Ferris on 2017-06-04.
+ *
+ */
+interface BasePresenter {
+
+    fun start()
+
+}
---
+++ app/src/main/java/ca/juqe/juqe/BaseView.kt
@@ -0,0 +1,11 @@
+package ca.juqe.juqe
+
+/**
+ * Created by Justin Ferris on 2017-06-04.
+ *
+ */
+interface BaseView<T> {
+
+    fun setPresenter(presenter: T)
+
+}
